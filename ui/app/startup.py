f9de18c1ce05dbc2c60d6e4f178dcf5f60d24113
Benjamin Pelletier <pelletier@alumni.caltech.edu>
2019-07-31 22:20:38 -0700

    Clean up for deployment

--- ui/app/startup.py
+++ ui/app/startup.py
@@ -1,11 +1,9 @@
 import logging
 
-from .lib.flaskapp import app
 from .lib import dbinit
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 log = logging.getLogger('streamtagger')
 
 log.info('Initializing database...')
-print('print init db', flush=True)
 dbinit.init_db()
